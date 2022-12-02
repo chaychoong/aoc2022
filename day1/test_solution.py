ab48f12157963318dedf5b405aead989e84037bf
Chay Choong <chaychoong@gmail.com>
2022-12-02 14:10:23 +0800

    util: add timing

--- day1/test_solution.py
+++ day1/test_solution.py
@@ -2,15 +2,18 @@ import os
 
 from day1.solution import get_most_calories, get_most_calories_top3
 from utils.get_input import get_input_as_list
+from utils.timing import timeit
 
 BASE_FOLDER = os.path.basename(os.path.dirname(__file__))
 
 
+@timeit
 def test_part1():
     assert get_most_calories(get_input_as_list(f"{BASE_FOLDER}/example.txt")) == 24000
     print(get_most_calories(get_input_as_list(f"{BASE_FOLDER}/puzzle.txt")))
 
 
+@timeit
 def test_part2():
     assert (
         get_most_calories_top3(get_input_as_list(f"{BASE_FOLDER}/example.txt")) == 45000
--- day2/test_solution.py
+++ day2/test_solution.py
@@ -2,15 +2,18 @@ import os
 
 from day2.solution import solution_p1, solution_p2
 from utils.get_input import get_input_as_list
+from utils.timing import timeit
 
 BASE_FOLDER = os.path.basename(os.path.dirname(__file__))
 
 
+@timeit
 def test_part1():
     assert solution_p1(get_input_as_list(f"{BASE_FOLDER}/example.txt")) == 15
     print(solution_p1(get_input_as_list(f"{BASE_FOLDER}/puzzle.txt")))
 
 
+@timeit
 def test_part2():
     assert solution_p2(get_input_as_list(f"{BASE_FOLDER}/example.txt")) == 12
     print(solution_p2(get_input_as_list(f"{BASE_FOLDER}/puzzle.txt")))
