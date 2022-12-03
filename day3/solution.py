fa0e711220d45d433b209a7e416257dbd2063825
Chay Choong <chaychoong@gmail.com>
2022-12-03 13:57:45 +0800

    day3: improve day3 solution using sets

--- day3/solution.py
+++ day3/solution.py
@@ -10,23 +10,15 @@ def split_rucksack(rucksack: str) -> Tuple[str, str]:
     return rucksack[:compartment_size], rucksack[compartment_size:]
 
 
-def get_common_item_in_two_lists(list1: List[str], list2: List[str]) -> str:
-    for item in list1:
-        if item in list2:
-            return item
+def get_common_item_in_n_lists(list_of_lists: List[List[str]]) -> str:
+    intersection = set.intersection(*map(set, list_of_lists))
 
-
-def get_common_item_in_three_lists(
-    list1: List[str], list2: List[str], list3: List[str]
-) -> str:
-    for item in list1:
-        if item in list2 and item in list3:
-            return item
+    return intersection.pop()
 
 
 def sum_common_priorities_in_rucksack(rucksack: str) -> int:
     compartment1, compartment2 = split_rucksack(rucksack)
-    return PRIORITY_LOOKUP[get_common_item_in_two_lists(compartment1, compartment2)]
+    return PRIORITY_LOOKUP[get_common_item_in_n_lists([compartment1, compartment2])]
 
 
 def solution_p1(rucksacks: List[str]) -> int:
@@ -36,20 +28,12 @@ def solution_p1(rucksacks: List[str]) -> int:
 # Part 2
 
 
-def get_common_item_in_three_lists(
-    list1: List[str], list2: List[str], list3: List[str]
-) -> str:
-    for item in list1:
-        if item in list2 and item in list3:
-            return item
-
-
 def solution_p2(rucksacks: List[str]) -> int:
     rucksacks_split = [
         [list(r) for r in rucksacks[i : i + 3]] for i in range(0, len(rucksacks), 3)
     ]
 
     return sum(
-        PRIORITY_LOOKUP[get_common_item_in_three_lists(*rucksack)]
+        PRIORITY_LOOKUP[get_common_item_in_n_lists(rucksack)]
         for rucksack in rucksacks_split
     )
